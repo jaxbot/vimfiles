b04ae1e9ad6b4b81c2525a8163816b7113a88742
Jonathan Warner <jaxbot@gmail.com>
2014-12-27 22:24:58 -0600

    Don't downscale on upload, do it on view instead

--- upload-screenshot.sh
+++ upload-screenshot.sh
@@ -1,6 +1,4 @@
 #!/bin/zsh
 screencapture -i /tmp/screenshot
-convert /tmp/screenshot -resize 50% /tmp/screenshot-half
-open `curl -F "filename=upload.jpg;image/jpeg;" -F "imagedata=@/tmp/screenshot-half" https://jaxbot.me/share.php\?token\=\`cat ~/.vim/.token\``
+open `curl -F "filename=upload.jpg;image/jpeg;" -F "imagedata=@/tmp/screenshot" https://jaxbot.me/share.php\?token\=\`cat ~/.vim/.token\``
 rm /tmp/screenshot
-rm /tmp/screenshot-half
