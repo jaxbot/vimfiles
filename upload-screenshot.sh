8529eea7a0c4baa6907d2a710bbd8c6435ce9600
Jonathan Warner <jaxbot@gmail.com>
2014-12-29 21:49:34 -0600

    Stop upload if cancelled

--- upload-screenshot.sh
+++ upload-screenshot.sh
@@ -1,4 +1,6 @@
 #!/bin/zsh
-screencapture -i /tmp/screenshot
-open `curl -F "filename=upload.jpg;image/jpeg;" -F "imagedata=@/tmp/screenshot" https://jaxbot.me/share.php\?token\=\`cat ~/.vim/.token\``
-rm /tmp/screenshot
+if screencapture -i /tmp/screenshot
+then
+	open `curl -F "filename=upload.jpg;image/jpeg;" -F "imagedata=@/tmp/screenshot" https://jaxbot.me/share.php\?token\=\`cat ~/.vim/.token\``
+	rm /tmp/screenshot
+fi
